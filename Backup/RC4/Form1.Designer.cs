f12e2bd0b342bdbf9c08f86e684b54e1b62fa1f9
nugmani <livesmitty@gmail.com>
2017-01-16 19:11:00 +0500

    ver.1

---
+++ Backup/RC4/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RC4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.key_text = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_result = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(121, 10);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(100, 20);
+            this.text.TabIndex = 0;
+            this.text.Text = "sella";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Шифруемый текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ключ";
+            // 
+            // key_text
+            // 
+            this.key_text.Location = new System.Drawing.Point(121, 40);
+            this.key_text.Name = "key_text";
+            this.key_text.Size = new System.Drawing.Size(100, 20);
+            this.key_text.TabIndex = 0;
+            this.key_text.Text = "no";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Результат";
+            // 
+            // text_result
+            // 
+            this.text_result.Location = new System.Drawing.Point(15, 109);
+            this.text_result.Multiline = true;
+            this.text_result.Name = "text_result";
+            this.text_result.Size = new System.Drawing.Size(246, 142);
+            this.text_result.TabIndex = 4;
+            this.text_result.TextChanged += new System.EventHandler(this.text_result_TextChanged);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(15, 267);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 5;
+            this.buttonGo.Text = "Начать";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(186, 267);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(186, 299);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 334);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.text_result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key_text);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "Главное окно";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox key_text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_result;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Backup/RC4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RC4
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            String temp_key = key_text.Text;
+            byte[] key = Encoding.GetEncoding("windows-1251").GetBytes(temp_key); // ASCIIEncoding.ASCII.GetBytes(temp_key);
+
+            RC4 encoder = new RC4(key);
+            String temp_text = text.Text;
+            byte[] testBytes = Encoding.GetEncoding("windows-1251").GetBytes(temp_text);
+            byte[] result = encoder.Encode(testBytes, testBytes.Length);
+            string crypt = Encoding.GetEncoding("windows-1251").GetString(result);
+            RC4 decoder = new RC4(key);
+            byte[] decryptedBytes = decoder.Decode(result, result.Length);
+            string decryptedString = Encoding.GetEncoding("windows-1251").GetString(decryptedBytes);
+
+            text_result.Text = "Зашифрованный текст:" + crypt+ "\r\nРасшифрованный текст:" + decryptedString + '\n';
+            
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            text_result.Clear();
+        }
+
+        private void text_result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+    }
+}
